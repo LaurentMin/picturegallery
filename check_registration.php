e702a672b766745ed2dca7898fcc2a537d01d4e4
Laurent MINATCHY <116948452+LaurentMin@users.noreply.github.com>
2025-12-10 10:05:00 +0100

    Fix vuln 3

--- check_registration.php
+++ check_registration.php
@@ -18,7 +18,12 @@ else{
 
 
     $sql1 = "SELECT users_username, users_password FROM users WHERE users_username = '{$username}' AND users_password = '{$password}'";
-    $sql2 = "INSERT INTO users (users_username, users_password) VALUES ('$username', '$password')";
+    $sql2 = "INSERT INTO users (users_username, users_password) VALUES (:username, :password)";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([
+	    ':username' => $username,
+	    ':password' => $password
+	]);
 
     $result1 = mysqli_query ($connection, $sql1) or die (mysqli_error ($connection));
 
